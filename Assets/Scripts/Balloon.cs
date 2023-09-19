ba93cfb89b0b2bb3bf2a4dddd8c0b4195682e0f3
LieT233 <f020628@gmail.com>
2023-09-19 19:10:39 -0400

    ballons

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -9,4 +9,26 @@ public class Balloon : MonoBehaviour
         // Add additional logic above here.
         Destroy(gameObject);
     }
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private Vector3 dir = Vector3.up;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (spriteRenderer.isVisible)
+        {
+            // 物体在摄像机视野内，执行你的函数
+            YourFunction();
+        }
+    }
+
+    void YourFunction()
+    {
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
 }
