8b977927ec85cc46b4b5bb67803babb57ac7eab5
luchsj <luchsj@rpi.edu>
2023-09-19 11:14:37 -0400

    Added balloon script, popping behavior.

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public void Pop()
+    {
+        // Add additional logic above here.
+        Destroy(gameObject);
+    }
+}
