1826d66307291d3fe8fb53b778e416140a1af0e7
LieT233 <f020628@gmail.com>
2023-09-19 20:25:35 -0400

    load scene edit

--- Assets/Scripts/SceneMan.cs
+++ Assets/Scripts/SceneMan.cs
@@ -18,17 +18,11 @@ public class SceneMan : MonoBehaviour
         SceneManager.LoadScene("Fail");  
     }
 
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+             SceneManager.LoadScene("Level_1_Building"); 
     }
 }
